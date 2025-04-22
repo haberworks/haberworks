b6de95468f2c24838cb001af2088f0dd4d6b950e
haberworks <142541927+haberworks@users.noreply.github.com>
2025-04-22 13:59:34 -0500

    Update page.tsx
    fix apostrophes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,12 +19,12 @@ export default function AboutPage() {
       <div className="about-container">
         <div className="about-content">
           <p className="about-paragraph">
-            Hey, Justin here. Welcome to my lil vibe-coded website. Just messin' around with a prototying process. At work, I do product management.
-            Outside of work, maybe the same thing?  I’ve spent over a decade in tech, working
+            Hey, Justin here. Welcome to my lil vibe-coded website. Just messin&apos; around with a prototying process. At work, I do product management.
+            Outside of work, maybe the same thing?  I&apos;ve spent over a decade in tech, working
             with some awesome people on some interesting projects, and formed lifelong friendships in the process, forged in the fires of start-ups and top-downs. 
           </p>
           <p className="about-paragraph">
-            Outside of work, I’m an avid tinkerer and thinkerer. See, here I am messing around with stuff I may not fully understand. But that is life. 
+            Outside of work, I&apos;m an avid tinkerer and thinkerer. See, here I&apos;m messing around with stuff I may not fully understand. But that is life. 
             Stuff I may know a bit more about includes my loving family and friends, my incredible fiancée Ashlynn (a real developer who rightfully judges my hack-work) and our cat Butters (who just wants to eat), my extensive collection of bass guitars that I <a href="https://www.instagram.com/jhawkins.nightshift/" target="_blank">still play sometimes</a>,
             my Steam library which continues to increase in size and dust accumulation, baseball (Let's Go Mets!), <a href="https://boz-float.itch.io/" target="_blank">making music for indie games</a>, <a href="https://gibby.games/" target="_blank">marketing for home grown board games</a>, and making tasty creations for the people I love in my kitchen. I am incredibly domesticated but don't often feel like it. 
             Maybe I feel right at home in that state of in-between. 
